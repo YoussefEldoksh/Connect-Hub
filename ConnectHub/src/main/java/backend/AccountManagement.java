79d7aa947aad1eae99e3650ab02e32e4a26ce5c3
HalaaHaytham <halaahaitham2@gmail.com>
2024-12-04 11:37:16 +0200

    second commit with changes to account management to implement in Frontend

--- ConnectHub/src/main/java/backend/AccountManagement.java
+++ ConnectHub/src/main/java/backend/AccountManagement.java
@@ -24,57 +24,55 @@ public class AccountManagement {
     
     ArrayList<User> users = FileManagement.loadFromJSONfile();
     
-    public User signUp(String userId, String email, String username, String password, LocalDate dateOfBirth, boolean status) //create a user
+    public int signUp(String userId, String email, String username, String password, LocalDate dateOfBirth, boolean status) //create a user
     {
         EmailValidator validator = EmailValidator.getInstance();
         if(!validator.isValid(email)) // check for email format
         {
             System.out.println("Invalid email format");
-            return null;
+            return 1;
         }
-        for (User user : users) // validate if the user already exists or uses a username or email that is already used
+        for (int i=0; i< users.size() ; i++) // validate if the user already exists or uses a username or email that is already used
         {
-            if(user.getUserId().equals(userId))
+            if(users.get(i).getUserId().equals(userId))
             {
                 System.out.println("User Already exists");
-                return null;
+                return 2;
             }
-            if(user.getEmail().equals(email))
+            if(users.get(i).getEmail().equals(email))
             {
                 System.out.println("Email Already used");
-                return  null;
+                return  3;
             }
             
         }
-        
-        
-
         User user = new User(userId, email, username, password, dateOfBirth, status);// created a new user
         users.add(user); //adding a new user to our data base
-        //here I'll add the user to the json file before I return the user
+        //here I'll add the user to the json file before I return that it was successfully added
         FileManagement.saveInJSONfile(users);
-        return user; //returned the new user for use
+        return 4; //returned the new user for use
     }
     
-    public boolean signIn(User user, ArrayList<User> users)
-    {
-        if(users.contains(user)) //check if the user is in our database
-        {
-            int index = users.indexOf(user);  // get indix of that user to compare
-            if(index == -1)
+    public User signIn(String unOrem, String password) {
+        for (int i = 0; i < users.size(); i++) {
+            if ((users.get(i).getUsername().equals(unOrem) || users.get(i).getEmail().equals(unOrem)) && (users.get(i).getPassword().equals(password))) //check if the user is in our database
             {
-                return false;
+                return users.get(i);
             }
-            
-            if((user.getUsername().equals(users.get(index).getUsername()) || user.getEmail().equals(users.get(index).getEmail())) && user.getPassword().equals(users.get(index).getPassword()))
-            {
-                System.out.println("User varified");
-                return true;
+
+        }
+        return null;
+    }
+    
+    public User findUser(String username) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUsername().equals(username)) {
+                System.out.println("User found");
+                return users.get(i);
             }
         }
-        return false;
+        return null;
     }
-    
     
     public void signOut(User user)
     {
